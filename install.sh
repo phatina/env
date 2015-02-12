c2e160a7ec6b8f5f99b3e133075c28d9c4e92316
Peter Hatina <phatina@gmail.com>
2015-02-12 08:57:16 +0100

    zsh: use oh-my-zsh

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@
 [ -L $HOME/.tmux.conf ] || ln -s $PWD/.tmux.conf $HOME
 [ -L $HOME/.vim ] || ln -s $PWD/.vim $HOME
 [ -L $HOME/.vimrc ] || ln -s $PWD/.vimrc $HOME
-[ -L $HOME/.zsh ] || ln -s $PWD/.zsh $HOME
 [ -L $HOME/.zshalias ] || ln -s $PWD/.zshalias $HOME
 [ -L $HOME/.zshenv ] || ln -s $PWD/.zshenv $HOME
-[ -L $HOME/.zshrc ] || ln -s $PWD/.zshrc $HOME
+
+# For now, let's use Oh My ZSH
+#[ -L $HOME/.zsh ] || ln -s $PWD/.zsh $HOME
+#[ -L $HOME/.zshrc ] || ln -s $PWD/.zshrc $HOME
+[ -L $HOME/.oh-my-zsh ] || ln -s $PWD/oh-my-zsh $HOME/.oh-my-zsh
+[ -L $HOME/.zshrc ] || ln -s $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,11 @@
 [ -L $HOME/.tmux.conf ] && unlink $HOME/.tmux.conf
 [ -L $HOME/.vim ] && unlink $HOME/.vim
 [ -L $HOME/.vimrc ] && unlink $HOME/.vimrc
-[ -L $HOME/.zsh ] && unlink $HOME/.zsh
 [ -L $HOME/.zshalias ] && unlink $HOME/.zshalias
 [ -L $HOME/.zshenv ] && unlink $HOME/.zshenv
+
+# For now, let's use Oh My ZSH
+#[ -L $HOME/.zsh ] && unlink $HOME/.zsh
+#[ -L $HOME/.zshrc ] && unlink $HOME/.zshrc
 [ -L $HOME/.zshrc ] && unlink $HOME/.zshrc
+[ -L $HOME/.oh-my-zsh ] && unlink $HOME/.oh-my-zsh
