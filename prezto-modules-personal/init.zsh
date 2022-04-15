cd9632707dbe0a248fba8feee5e3d4dd65aa1722
Peter Hatina <peter@hatina.eu>
2022-04-15 13:27:58 +0200

    ZSH: Update personal module
    Signed-off-by: Peter Hatina <peter@hatina.eu>

--- prezto-modules-personal/init.zsh
+++ prezto-modules-personal/init.zsh
@@ -13,8 +13,10 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
-alias gdbtui='gdb -tui'
-alias ipython='ipython --no-confirm-exit'
+# Tmux
+alias t="tmux"
+alias ta="t a -t"
+alias tls="t ls"
 
 # Git module mods
 alias ga='git add'
@@ -22,8 +24,15 @@ alias gc!='git commit --amend'
 alias gcm='git checkout master'
 alias gd='git diff'
 alias gst='git status'
+alias gbrr='git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done'
+alias gdbtui='gdb -tui'
+
+# Yocto
+alias bb='bitbake'
 
 # Function definitions
 function mkcd {
     mkdir -p $1 && cd $1
 }
+
+PATH=$HOME/Work/cpi/bin:$PATH
